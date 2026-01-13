9ba0430b42ed43b6a02f65e0d7c6cf0131695b24
GoGradually <chkun3109@gmail.com>
2026-01-13 15:33:10 +0900

    build: 초기 프로젝트 설정 및 Gradle 구성 추가

---
+++ src/main/java/me/gogradually/fcmnotificationtest/FcmNotificationTestApplication.java
@@ -0,0 +1,13 @@
+package me.gogradually.fcmnotificationtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FcmNotificationTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FcmNotificationTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/gogradually/fcmnotificationtest/FcmNotificationTestApplicationTests.java
@@ -0,0 +1,13 @@
+package me.gogradually.fcmnotificationtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FcmNotificationTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

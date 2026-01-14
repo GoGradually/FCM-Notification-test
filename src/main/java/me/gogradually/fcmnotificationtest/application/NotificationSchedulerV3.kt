986c1ad75a1f91500ecaf2d25a4fc9dc3b34ed7d
GoGradually <chkun3109@gmail.com>
2026-01-14 17:57:07 +0900

    feat: NotificationSchedulerV4에 가상 스레드 도입 및 실행 시간 로깅 추가

--- src/main/java/me/gogradually/fcmnotificationtest/application/NotificationSchedulerV3.kt
+++ src/main/java/me/gogradually/fcmnotificationtest/application/NotificationSchedulerV3.kt
@@ -3,9 +3,11 @@ package me.gogradually.fcmnotificationtest.application
 import me.gogradually.fcmnotificationtest.common.LogElapsedTime
 import me.gogradually.fcmnotificationtest.domain.PushSubscriptionRepository
 import me.gogradually.fcmnotificationtest.infrastrcuture.MockFcmServiceV3
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import java.util.concurrent.Future
+import kotlin.system.measureTimeMillis
 
 /**
  * V1 + 배치 처리로 네트워크 I/O 횟수를 줄인 버전
@@ -15,26 +17,31 @@ class NotificationSchedulerV3(
     private val pushSubscriptionRepository: PushSubscriptionRepository, private val pushService: MockFcmServiceV3
 ) {
 
+    val logger = LoggerFactory.getLogger(NotificationSchedulerV4::class.java)
+
     @LogElapsedTime
     @Transactional
     fun dispatchDueNotifications() {
         val dueNotificationOwners = (0 until 2000L).toList()
-
-        val futures = mutableListOf<Future<*>>()
-
-        dueNotificationOwners.forEach {
-            futures.addAll(pushService.sendNotificationToOwner(it))
-        }
-
         val tokens = mutableListOf<String>()
-        for (future in futures) {
-            try {
-                future.get()
-            } catch (e: Exception) {
-                val failedToken = e.cause?.message ?: continue
-                tokens.add(failedToken)
+        val measureTimeMillis = measureTimeMillis {
+            val futures = mutableListOf<Future<*>>()
+
+            dueNotificationOwners.forEach {
+                futures.addAll(pushService.sendNotificationToOwner(it))
+            }
+
+            for (future in futures) {
+                try {
+                    future.get()
+                } catch (e: Exception) {
+                    val failedToken = e.cause?.message ?: continue
+                    tokens.add(failedToken)
+                }
             }
         }
+
+        logger.info("Total dispatch time: {} ms", measureTimeMillis)
 
         pushSubscriptionRepository.deleteAllByTokenInBatch(tokens)
     }

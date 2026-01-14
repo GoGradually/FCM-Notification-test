dc79efa7122a2c0be98645ab19eb02d1fa577d2e
GoGradually <chkun3109@gmail.com>
2026-01-14 14:23:33 +0900

    feat: 푸시 알림 전송 기능 스레드 풀을 통한 비동기 처리 구현

--- src/main/java/me/gogradually/fcmnotificationtest/application/NotificationSchedulerV1.kt
+++ src/main/java/me/gogradually/fcmnotificationtest/application/NotificationSchedulerV1.kt
@@ -1,33 +1,35 @@
 package me.gogradually.fcmnotificationtest.application
 
-import me.gogradually.fcmnotificationtest.domain.PushSubscription
 import me.gogradually.fcmnotificationtest.domain.PushSubscriptionRepository
 import me.gogradually.fcmnotificationtest.infrastrcuture.MockFcmServiceV1
 import org.springframework.stereotype.Service
-import org.springframework.transaction.annotation.Transactional
+import java.util.concurrent.Future
+import java.util.concurrent.LinkedBlockingQueue
+import java.util.concurrent.ThreadPoolExecutor
+import java.util.concurrent.TimeUnit
 
 @Service
 class NotificationSchedulerV1(
-    private val pushSubscriptionRepository: PushSubscriptionRepository,
-    private val pushService: MockFcmServiceV1
+    private val pushSubscriptionRepository: PushSubscriptionRepository, private val pushService: MockFcmServiceV1
 ) {
 
-    @Transactional
+    val threadPoolExecutor = ThreadPoolExecutor(100, 100, 60L, TimeUnit.SECONDS, LinkedBlockingQueue())
+
+    init {
+        threadPoolExecutor.allowCoreThreadTimeOut(true)
+    }
+
     fun dispatchDueNotifications() {
         val dueNotificationOwners = (0 until 2000L).toList()
 
-        dueNotificationOwners.forEach { owner ->
-            sendNotificationToOwner(owner)
+        val futures = mutableListOf<Future<*>>()
+
+        dueNotificationOwners.forEach {
+            futures.add(threadPoolExecutor.submit {
+                pushService.sendNotificationToOwner(it)
+            })
         }
-    }
 
-
-    private fun sendNotificationToOwner(ownerId: Long) {
-        val subscriptions =
-            pushSubscriptionRepository.findAllByMemberId(ownerId)
-
-        subscriptions.forEach { subscription: PushSubscription ->
-            pushService.sendPushMessage(subscription.token, ownerId)
-        }
+        futures.forEach { it.get() }
     }
 }
--- src/main/java/me/gogradually/fcmnotificationtest/infrastrcuture/MockFcmServiceV1.kt
+++ src/main/java/me/gogradually/fcmnotificationtest/infrastrcuture/MockFcmServiceV1.kt
@@ -1,9 +1,11 @@
 package me.gogradually.fcmnotificationtest.infrastrcuture
 
 import me.gogradually.fcmnotificationtest.application.PushService
+import me.gogradually.fcmnotificationtest.domain.PushSubscription
 import me.gogradually.fcmnotificationtest.domain.PushSubscriptionRepository
 import me.gogradually.fcmnotificationtest.external.MockFcmProcessor
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 
 @Service
@@ -13,11 +15,23 @@ class MockFcmServiceV1(
 ) :
     PushService {
 
+
     override fun sendPushMessage(token: String, ownerId: Long) {
         try {
             mockFcmProcessor.sendMessage(token, ownerId)
         } catch (e: Exception) {
             pushSubscriptionRepository.deleteByToken(token)
+        }
+    }
+
+
+    @Transactional
+    fun sendNotificationToOwner(ownerId: Long) {
+        val subscriptions =
+            pushSubscriptionRepository.findAllByMemberId(ownerId)
+
+        subscriptions.forEach { subscription: PushSubscription ->
+            sendPushMessage(subscription.token, ownerId)
         }
     }
 }

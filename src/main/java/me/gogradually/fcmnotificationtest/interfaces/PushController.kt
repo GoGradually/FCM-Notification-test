e9ceeb8db09365c69968888648235e4bbe1570eb
GoGradually <chkun3109@gmail.com>
2026-01-14 12:16:00 +0900

    feat: PushController의 테스트 엔드포인트 버전 관리

--- src/main/java/me/gogradually/fcmnotificationtest/interfaces/PushController.kt
+++ src/main/java/me/gogradually/fcmnotificationtest/interfaces/PushController.kt
@@ -2,11 +2,13 @@ package me.gogradually.fcmnotificationtest.interfaces
 
 import me.gogradually.fcmnotificationtest.application.NotificationScheduler
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@RequestMapping("/test")
 class PushController(private val notificationScheduler: NotificationScheduler) {
-    @GetMapping("/test")
+    @GetMapping("/v0")
     fun test() {
         notificationScheduler.dispatchDueNotifications()
     }
